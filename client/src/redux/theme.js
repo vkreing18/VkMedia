1bcfd1466874d560fb163a57bba8afbea6970293
Mirza Sahid Baig <128984287+vkreing18@users.noreply.github.com>
2023-11-26 22:51:01 +0530

    Add files via upload

---
+++ client/src/redux/theme.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  theme: JSON.parse(window?.localStorage.getItem("VkMedia-theme")) ?? "light",
+};
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState,
+  reducers: {
+    setTheme(state, action) {
+      state.theme = action.payload;
+      localStorage.setItem("VkMedia-theme", JSON.stringify(action.payload));
+    },
+  },
+});
+
+export default themeSlice.reducer;
+
+export function SetTheme(value) {
+  return (dispatch) => {
+    dispatch(themeSlice.actions.setTheme(value));
+  };
+}
